a951b2357ce23050681fb429b73db6851d5e0901
Dr Koppula Vijaya Kumar <154619192+Vijaykoppula5@users.noreply.github.com>
2023-12-29 09:34:26 +0000

    ver1

--- t1.py
+++ t1.py
@@ -4,3 +4,5 @@ with open('data.txt', 'r') as file:
     # Use Copilot to complete the code to read and print each line with line numbers
     for line in file:
         # TODO: Complete the code here
+        for line_number, line in enumerate(file, start=1):
+        print(f"Line {line_number}: {line.strip()}")
